6e28c1e2eb52ec5532d227561d7154bea9f46d48
renu-kumawat <58653958+renu-kumawat@users.noreply.github.com>
2021-05-10 19:34:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 song="";
+leftwristX=0;
+leftwristY=0;
+RightwristX=0;
+RightwristY=0;
 function setup(){
 canvas=createCanvas(600,500);
 canvas.center();
 video=createCapture(VIDEO);
 video.hide();
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        leftwristX=results[0].pose.leftWrist.x;
+        leftwristY=results[0].pose.leftWrist.y;
+        console.log(leftwristX,leftwristY);
+        RightwristX=results[0].pose.rightWrist.x;
+        RightwristY=results[0].pose.rightWrist.y;
+        console.log(RightwristX,RightwristY);
+    }
 }
 function draw(){
 image(video,0,0,600,500);
